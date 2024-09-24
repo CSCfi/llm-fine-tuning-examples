b61c2ced5f36b4ca5773456255d1a7555740b901
Mats Sjöberg <mats.sjoberg@csc.fi>
2024-09-24 10:03:08 +0300

    Add inference script for mahti

---
+++ run-inference-mahti.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --partition=gputest
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=80G
+#SBATCH --time=0:15:00
+#SBATCH --gres=gpu:a100:1
+
+module purge
+module load pytorch/2.4
+
+# This will store all the Hugging Face cache such as downloaded models
+# and datasets in the project's scratch folder
+export HF_HOME=/scratch/${SLURM_JOB_ACCOUNT}/${USER}/hf-cache
+mkdir -p $HF_HOME
+
+# Path to where the trained model and logging data will go
+OUTPUT_DIR=/scratch/${SLURM_JOB_ACCOUNT}/${USER}/hf-data
+mkdir -p $OUTPUT_DIR
+
+# Disable internal parallelism of huggingface's tokenizer since we
+# want to retain direct control of parallelism options.
+export TOKENIZERS_PARALLELISM=false
+
+set -xv  # print the command so that we can verify setting arguments
+         # correctly from the logs
+
+srun python3 inference-demo.py $*
